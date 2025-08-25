02223e3466663569200e6cf207f2fabac265d305
anurag357 <89999226+anurag357@users.noreply.github.com>
2025-08-25 18:54:43 +0530

    Re-add project files without secrets

---
+++ README.md
@@ -0,0 +1,12 @@
+## GeminiAI Studio =  https://aistudio.google.com/prompts/new_chat
+## https://ai.google.dev/gemini-api/docs/openai#javascript
+## Claude SDK = https://docs.anthropic.com/en/docs/claude-code/sdk
+## Zero sort prompting -> The model is given a direct question or task without any prior.
+## Few Shot Prompting -> you givenan exmaple to the LLM
+## Chain of Thought -> The model is incouraged to break down resoning step by step arrieving to the answers
+## Selt Consistency Prompting -> The model genrate multiple responses and selects the most consistency answers.
+## Persona Based Prompting -> The model is intructed to respond as if it were a particuler charector or professtional.
+## GiminiAI ClI
+## Curser CLI
+## Cluad CLI
+## Perplexcity CLI
---
+++ prompting/persona.js
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+import { OpenAI } from 'openai';
+
+
+
+async function main() {
+  // These api calls are stateless (Zero Shot)
+  const response = await client.chat.completions.create({
+    model: 'gpt-4.1-mini',
+    messages: [
+      {
+        role: 'system',
+        content: `
+                You are an AI assistant who is Vikash. You are a persona of a developer named
+                Vikash who is an amazing developer and codes in React and Javascipt.
+
+                Characteristics of Vikash
+                - Full Name: Vikash
+                - Age: 25 Years old
+                - Date of birthday: 7th Jan, 2000
+
+                Social Links:
+                - LinkedIn URL: 
+                - X URL: 
+
+                Examples of text on how Vikash typically chats or replies:
+                - Hey Anurag, Yes
+                - This can be done.
+                - Sure, I will do this
+                
+            `,
+      },
+      { role: 'user', content: 'Hey gpt, My name is Anurag Pathak' },
+    ],
+  });
+
+  console.log(response.choices[0].message.content);
+}
+
+main();
